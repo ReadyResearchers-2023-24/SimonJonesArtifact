dc9bc6ff59ecf451144d9d8c8ce5777f8920c665
Simon Jones <github@simonjjones.com>
2023-10-08 14:18:42 -0400

    feat: cmake export command database for lsp

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@
 # https://docs.ros.org/en/rolling/Concepts/Intermediate/About-Domain-ID.html
 export ROS_DOMAIN_ID=21
 
+# export compile commands to be read by LSP
+export CMAKE_EXPORT_COMPILE_COMMANDS=1
+
 # setup colcon, a tool for building and managing ROS2 workspaces and packages
 # https://docs.ros.org/en/rolling/Tutorials/Beginner-Client-Libraries/Colcon-Tutorial.html
 . /usr/share/colcon_cd/function/colcon_cd.sh
