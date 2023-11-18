9313ad2505138d9b45fdb47680e0a322ef11650f
Simon Jones <github@simonjjones.com>
2023-11-17 21:42:30 -0500

    feat: using os.path.join() for launch files

--- simulation_nodes.py
+++ simulation_nodes.py
@@ -1,5 +1,7 @@
-import rospy
+import os
+
 import roslaunch
+import rospy
 
 px4_launch = None
 gazebo_launch = None
@@ -9,21 +11,30 @@ clover_model_launch = None
 def launch_px4():
     """Launch px4 node."""
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-    px4_launch = roslaunch.parent.ROSLaunchParent(uuid, ["./px4.launch"])
+    px4_launch = roslaunch.parent.ROSLaunchParent(
+        uuid,
+        [os.path.join("launch", "px4.launch")]
+    )
     px4_launch.start()
     rospy.loginfo("started px4")
 
 def launch_gazebo():
     """Launch gazebo node."""
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-    gazebo_launch = roslaunch.parent.ROSLaunchParent(uuid, ["./gazebo.launch"])
+    gazebo_launch = roslaunch.parent.ROSLaunchParent(
+        uuid,
+        [os.path.join("launch", "gazebo.launch")]
+    )
     gazebo_launch.start()
     rospy.loginfo("started gazebo")
 
 def launch_clover_services():
     """Launch clover_services node."""
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-    clover_services_launch = roslaunch.parent.ROSLaunchParent(uuid, ["./clover_services.launch"])
+    clover_services_launch = roslaunch.parent.ROSLaunchParent(
+        uuid,
+        [os.path.join("launch", "clover_services.launch")]
+    )
     clover_services_launch.start()
     rospy.loginfo("started clover_services")
 
@@ -31,7 +42,10 @@ def launch_clover_model():
     """Launch clover_model node."""
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-    clover_model_launch = roslaunch.parent.ROSLaunchParent(uuid, ["./clover_model.launch"])
+    clover_model_launch = roslaunch.parent.ROSLaunchParent(
+        uuid,
+        [os.path.join("launch", "clover_model.launch")]
+    )
     clover_model_launch.start()
     rospy.loginfo("started clover_model")
 
